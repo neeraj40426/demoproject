db9eeadfe1d6deaab3cfc9a124a2475efcd92949
neeraj40426 <neeraj40426@gmail.com>
2026-03-12 23:16:38 +0530

    fixed index.js for port

--- demomodel/server/index.js
+++ demomodel/server/index.js
@@ -86,6 +86,8 @@ io.to(roomId).emit("stateUpdate",rooms[roomId]);
 }
 },50);
 
-server.listen(3000,()=>{
-console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port", PORT);
 });
